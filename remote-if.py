29c16dbc144cd4cab0aa4d614a4ab56e154de2fc
Andrew Plotkin <erkyrath@eblong.com>
2013-10-09 23:08:40 -0400

    The play page.

--- remote-if.py
+++ remote-if.py
@@ -43,7 +43,7 @@ class MainHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def post(self):
         if self.get_argument('signin', None):
-            sessionid = binascii.hexlify(os.urandom(16))
+            sessionid = binascii.hexlify(os.urandom(16)) # bytes, not string
             self.set_secure_cookie('sessionid', sessionid, expires_days=10)
         elif self.get_argument('signout', None):
             sessionid = None
@@ -52,9 +52,31 @@ class MainHandler(tornado.web.RequestHandler):
             raise Exception('Unknown form button')
         self.render('main.html', sessionid=sessionid)
         
+class PlayHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        sessionid = self.get_secure_cookie('sessionid')
+        if not sessionid:
+            raise Exception('You are not logged in')
+        
+        session = self.application.sessions.get(sessionid)
+        if not session:
+            session = Session(sessionid)
+            self.application.sessions[sessionid] = session
+            self.application.log.info('Created session object %s', session)
+            
+        self.render('play.html')
+
+class Session:
+    def __init__(self, sessionid):
+        self.id = sessionid
+    def __repr__(self):
+        return '<Session "%s">' % (self.id.decode(),)
+        
 # Core handlers.
 handlers = [
     (r'/', MainHandler),
+    (r'/play', PlayHandler),
 ]
 
 class MyApplication(tornado.web.Application):
@@ -63,7 +85,10 @@ class MyApplication(tornado.web.Application):
     """
     def init_app(self):
         # Grab the same logger that tornado uses.
-        self.twlog = logging.getLogger("tornado.general")
+        self.log = logging.getLogger("tornado.general")
+
+        # Maps session ID to session objects.
+        self.sessions = {}
 
 application = MyApplication(
     handlers,
